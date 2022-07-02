798f5291a03955f78fea45f1075f10587bf73bff
Manu Pranav <manupranav62@gmail.com>
2022-07-01 19:30:49 -0700

    commit2

--- base/views.py
+++ base/views.py
@@ -2,17 +2,16 @@ from django.shortcuts import render
 from .models import Room
 
 
-rooms = [
-    {'id':1,'name':'Lets learn python!' },
-    {'id':2,'name':'Lets learn C!' },
-    {'id':3,'name':'Lets learn C++!' },
-    ]
+
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms':rooms}
     return render(request, 'base/home.html',context)
 
 
 def room(request, pk):
-    return render(request, 'base/room.html')
+    room = Room.objects.get(id=pk)
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
 
