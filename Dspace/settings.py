88cb221d709f021b5c55bf95784968bb9a9b3f35
manupranav <62414791+manupranav@users.noreply.github.com>
2022-07-10 21:15:48 -0700

    rest

--- Dspace/settings.py
+++ Dspace/settings.py
@@ -37,10 +37,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'base.apps.BaseConfig',
+
+    'rest_framework',
+
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
